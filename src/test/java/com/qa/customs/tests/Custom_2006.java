4458d3218effa312c93f833296e8757b200e8431
Ajay <Ajayp9594@gmail.com>
2020-04-13 19:55:08 +0530

    Added code for custom-2006

---
+++ src/test/java/com/qa/customs/tests/Custom_2006.java
@@ -0,0 +1,23 @@
+package com.qa.customs.tests;
+
+import org.testng.annotations.Test;
+
+public class Custom_2006 {
+
+	
+	@Test
+	public void setup() {
+		
+		System.out.println("Running setup for tests");
+		System.out.println("Performing Initialization");
+	}
+	
+	@Test
+	public void customFeatureTest() {
+		
+		
+		System.out.println("Running code to check functionality of the "
+				+ "feature added in custom-2006");
+	}
+	
+}
